24b9035c4a5ceadaefb8d2d262a52bb7968c12a7
divija <dbharga@uwo.ca>
2022-01-08 18:31:08 -0500

    added to feed.js

--- feed.js
+++ feed.js
@@ -1,2 +1,21 @@
 import React from "react";
 import "./Feed.css";
+// import CreateIcon from 
+
+function feed(){
+    return(
+        <div className='feed'>
+            <div className="feed_inputContainer">
+                <div classname="feed_input">
+                    <CreateIcon/>
+                    <form>
+                        <input type="text"/>
+                        
+                    </form>
+                 </div>
+            </div>
+        
+        </div>
+
+    )
+}
